b6bf65137eb6e64acf0b939a991c22edd9699c0f
Mark Tearle <mark.tearle@qcif.edu.au>
2026-05-28 14:08:22 +0800

    Add cookie setting logic

--- ckanext/lollipop/views.py
+++ ckanext/lollipop/views.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 from flask.views import MethodView
-from ckan.common import _, g
+from ckan.common import _, g, config
 from ckan.lib.base import render, abort, request
 from ckan.logic import get_action, ValidationError, NotFound, NotAuthorized
 import ckan.logic as logic
@@ -9,13 +9,20 @@ import ckan.lib.helpers as h
 import ckan.lib.navl.dictization_functions as dict_fns
 
 from logging import getLogger
+from datetime import datetime, timedelta
 
 logger = getLogger(__name__)
 
 lollipop = Blueprint("lollipop", __name__)
 
 def lollipop_process():
+    cookie_name = config.get('ckanext.lollipop.cookie_name', 'ckanext-lollipop-yum')
+    cookie_expiry = int(config.get('ckanext.lollipop.cookie_expiry', 7))
+    cookie_value = "proceed with care"
+
     lollipop_status = 'bad'
+    expiry = 0
+
     try:
         data_dict = logic.clean_dict(
             dict_fns.unflatten(logic.tuplize_dict(logic.parse_params(request.form)))
@@ -31,18 +38,21 @@ def lollipop_process():
 
     try:
         captcha.check_recaptcha(request)
+        lollipop_status = 'good'
+        expiry = datetime.now() + timedelta(days=cookie_expiry)
     except captcha.CaptchaError:
         error_msg = _(u'Bad Captcha. Please try again.')
         h.flash_error(error_msg)
-        # FIXME Need to clear the cookie
-        return self.get(data_dict)
+        lollipop_status = 'bad'
+        expiry = 0
 
-    # FIXME Need to set the cookie
-    lollipop_status = 'good'
-
-    return h.redirect_to(
+    response =  h.redirect_to(
         data_dict.get("return_to", "home.index"), lollipop_status=lollipop_status
-   )
+    )
+
+    response.set_cookie(cookie_name, expires=expiry, value=cookie_value)
+
+    return response
 
 def lollipop_captcha_failed(self):
     return render("ckanext_lollipop/lollipop_captcha_failed.html", {})
