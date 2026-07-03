f97c5e45cbe0df55038f5f24b3d7400350f08fd9
Mark Tearle <mark.tearle@qcif.edu.au>
2026-07-03 16:35:23 +0800

    Remove duplicated code

--- ckanext/lollipop/action.py
+++ ckanext/lollipop/action.py
@@ -19,30 +19,13 @@ def _get_user_name(context, data_dict):
     return model.User.get(user).name
 
 
-
-@action
-@validate(schema.lollipop_process)
-def lollipop_process(context, data_dict):
-    tk.check_access("lollipop_process", context, data_dict)
-
-    # FIXME ensure not logged in here
-
-    valid = False
-    for impl in p.PluginImplementations(interface.ILollipop):
-        update = impl.lollipop_process(context, data_dict)
-        if valid:
-            return valid
-
-    return valid
-
-
 @action
 @validate(schema.lollipop_process)
 def lollipop_process(context, data_dict):
     tk.check_access("lollipop_process", context, data_dict)
 
     processed = False
-    for impl in p.PluginImplementations(interface.IAcceptableUse):
+    for impl in p.PluginImplementations(interface.ILollipop):
         processed = impl.lollipop_process(context, data_dict)
         if processed:
             return processed
