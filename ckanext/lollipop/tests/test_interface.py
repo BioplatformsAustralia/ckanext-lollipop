17d37aadd774d43ae6482af1adc91a0e95ba7f28
Mark Tearle <mark.tearle@qcif.edu.au>
2026-07-07 16:22:56 +0800

    Add tests for interface.py

--- ckanext/lollipop/tests/test_interface.py
+++ ckanext/lollipop/tests/test_interface.py
@@ -8,9 +8,39 @@ import ckanext.lollipop.interface as interface
 
 
 class TestLollipopInterface(object):
+    def test_lollipop_set(self):
+        lollipop = interface.ILollipop()
+
+        result = lollipop.lollipop_set(None)
+
+        assert result == True
+
+    def test_lollipop_update(self):
+        lollipop = interface.ILollipop()
+
+        result = lollipop.lollipop_update(None)
+
+        assert result == True
+
+    def test_lollipop_clear(self):
+        lollipop = interface.ILollipop()
+
+        result = lollipop.lollipop_clear(None)
+
+        assert result == True
+
     def test_lollipop_required(self):
         lollipop = interface.ILollipop()
 
         result = lollipop.lollipop_required()
+
+        assert result == True
+
+    def test_lollipop_process(self):
+        lollipop = interface.ILollipop()
+
+        context = {}
+        data_dict = {}
+        result = lollipop.lollipop_process(context, data_dict)
 
         assert result == True
