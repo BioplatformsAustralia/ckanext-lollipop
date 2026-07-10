a5ad7dac8b556a6f6872bfd5bb87010cde894da4
Mark Tearle <mark.tearle@qcif.edu.au>
2026-07-10 14:46:23 +0800

    Remove unused code in action.py and auth.py

--- ckanext/lollipop/action.py
+++ ckanext/lollipop/action.py
@@ -9,25 +9,3 @@ from ckanext.toolbelt.decorators import Collector
 from . import schema
 
 action, get_actions = Collector().split()
-
-
-def _get_user_name(context, data_dict):
-    if data_dict.get("user_id"):
-        return data_dict["user_id"]
-
-    user = context.get("user", None)
-    return model.User.get(user).name
-
-
-@action
-@validate(schema.lollipop_process)
-def lollipop_process(context, data_dict):
-    tk.check_access("lollipop_process", context, data_dict)
-
-    processed = False
-    for impl in p.PluginImplementations(interface.ILollipop):
-        processed = impl.lollipop_process(context, data_dict)
-        if processed:
-            return processed
-
-    return processed
